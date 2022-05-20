5ab781b97ec66a316a3e28271a0ba3a45e39511f
patwyzlic <patrickwyzlic@gmail.com>
2022-05-20 15:58:46 -0400

    Commit 1 - Sudo Code

---
+++ game.js
@@ -0,0 +1,49 @@
+/* 
+Penguin in minecart jumping over obstacles in 2d. (Inspired by club penguin cart surfer)
+
+Falling rocks(Ducking), 
+jumping minecart,
+tricks for extra points, 
+gaps in the tracks,
+barriers to jump over,
+ghost enemy
+
+Background scrolls right to left
+tracks at bottom of screen for the minecart
+
+Uses HTML, CSS, and JavaScript/ JS Canvas
+*/
+
+//Score
+
+//Win for highest score during session
+
+//Instructions page
+
+//Canvas function
+
+//Background of function
+
+//Background generator
+
+//Render function
+
+//Character class
+
+//Keyboard controls
+
+//Character lose function 
+
+//Falling rock function
+
+//Trick function
+
+/*Ghost enemy function:
+Either duck, or jump to get past them alive
+*/
+
+//Detect hit
+
+//Game loop
+
+//Back button

db12422bedf4361e118d6771c6d6e05a9f74d9f0
patwyzlic <patrickwyzlic@gmail.com>
2022-05-25 12:57:04 -0400

    Reverting to setTimeOut
    Falling rock started

--- game.js
+++ game.js
@@ -21,8 +21,6 @@ let ctx = game.getContext("2d");
 ctx.imageSmoothingEnabled = true;
 //Initialize penguin
 let penguin;
-//Initialize ducking penguin
-let duckingPenguin;
 //Initialize rails
 let rails;
 //Initialize barriers
@@ -34,8 +32,6 @@ let scoreNumber = 0;
 let keyboardMovement = document.querySelector('keyboard-movement');
 //Storage location for penguin image
 let penguinObj = new Image();
-//Storage location for ducking penguin image
-let duckingPenguinObj = new Image();
 //Storage location for rail image
 let railObj = new Image();
 //Storage location for barrier image
@@ -60,11 +56,6 @@ function(e){
         penguinObj.onload = function(){
             imageHandler(penguin, penguinObj, penguinX, penguinY, 32, 32);
         }
-        duckingPenguinObj.src = "/Cart-Penguin/Images/Ducking-Penguin.png";
-
-        duckingPenguinObj.onload = function(){
-            imageHandler(duckingPenguin, duckingPenguinObj, penguinX, penguinY, 32, 32);
-        }
         railObj.src = "/Cart-Penguin/Images/rails.png";
         railObj.onload = function(){
             //Makes game start with rails
@@ -88,20 +79,16 @@ function(e){
 });
 
 //Game loop
-let newInterval = setInterval(function(){
-    imageHandler(penguin, penguinObj, penguinX, penguinY, 32, 32);
-}, 1000);
-let alwaysOn = setInterval(function(){
-    generator();
-}, 1000);
-
 function gameLoop(){
     //SetInterval to run game loop once a second
+    setInterval(function() {
+        generator();
+        imageHandler(penguin, penguinObj, penguinX, penguinY, 32, 32);
         if(gameOver === true){
             gameLost();
-        }  
+        } 
+    }, 1000);   
 }
-
 //Class to hold generated boxes
 class GameObject {
     constructor(x, y, color, width, height){
@@ -146,15 +133,15 @@ let nextScene = true;
 let d = 0;
 //Holds all obstacles
 function generator(){
-    /*
         //New number generated for randomizing scene
-        let newRandom = Math.floor(Math.random() * 3);
+        let newRandom = Math.floor(Math.random() * 5);
         let randTime = Math.floor(Math.random() * 1000 + 1000);  
         let l = 9;
     //Barrier scene
     if(newRandom === 1 && nextScene === true && lastToGo[0] !== 'Barrier' && d > 4){
         nextScene = false;
         lastToGo.fill('Barrier', 0, 1);
+        console.log(lastToGo);
         for(let i = 0; i < 10; i++){  
             imageHandler(rails, railObj, 30, 100, 32, 32);
             hitLost(30*l, 99);
@@ -190,22 +177,28 @@ function generator(){
                     let v = (l + 1) * 30;
                     let back = new GameObject(30 * l, 120, 'black', 32, 32);
                     back.render();
-                    let backTwo = new GameObject(30 * l, 108, 'grey', 32, 20);
+                    let backTwo = new GameObject(30 * l, 110, 'grey', 32, 20);
+                    console.log(penguinX, penguinY);
                     hitLost(30*l, 99);
                     back.render();
                     backTwo.render();
+                    console.log(30*l, 110);
                         ctx.clearRect(v , 120, 32, 32);
                         if(l < 9){
                             if(l < 1){
                                 hitLost(30*l, 99);
                                 ctx.clearRect(0 , 120, 32, 32);
                                 imageHandler(rails, railObj, 0,  100, 
-                                32, 32)
+                                32, 32); 
                             }
                             imageHandler(rails, railObj, v,  100, 32, 32);
                         }
                         l--;
                 }
+                setInterval(() => {
+                    ctx.clearRect(30 , 73, 32, 32);
+                    imageHandler(penguin, penguinObj, penguinX, penguinY, 32, 32);
+                }, 50);
             }, i * 200);
         }
         setTimeout(() => {
@@ -235,83 +228,89 @@ function generator(){
                         }
                         l--;
                 }
+                setInterval(() => {
+                    ctx.clearRect(30 , 73, 32, 32);
+                    imageHandler(penguin, penguinObj, penguinX, penguinY, 32, 32);
+                }, 50);
             }, i * 200);
         }
         setTimeout(() => {
             nextScene = true;
         }, randTime);
     }
-    //Falling rock function*/
+    //Falling rock object
+    else if(newRandom < 4 && nextScene === true && lastToGo[0] !== 'rock' && d > 4){
+        lastToGo.fill('rock', 0, 1 && d > 5);
+        for(let i = 0; i < 10; i++){
+            setTimeout(() => {
+                if(scenes[i][l] === 2){
+                    let v = (l + 1) * 30;
+                    let back = new GameObject(30 * l, 27 * i * .5, '#524545', 20, 20);
+                    back.render();
+                    console.log(penguinX, penguinY);
+                    hitLost(30*l, 99);
+                    back.render();
+                    console.log(30*l, 110);
+                        //ctx.clearRect(v , 120, 32, 32);
+                        if(l < 9){
+                            if(l < 1){
+                                hitLost(30*l, 99);
+                                ctx.clearRect(0 , 120, 32, 32);
+                                imageHandler(rails, railObj, 0,  100, 
+                                32, 32); 
+                            }
+                            imageHandler(rails, railObj, v,  100, 32, 32);
+                        }
+                        l--;
+                }
+                setInterval(() => {
+                    ctx.clearRect(30 , 60, 32, 32);
+                    imageHandler(penguin, penguinObj, penguinX, penguinY, 32, 32);
+                }, 50);
+            }, i * 200);
+        }
+        setTimeout(() => {
+            scoreNumber++;
+            score.textContent = `Score: ${scoreNumber}`; 
+            nextScene = true;
+        }, randTime);
+        
+    }
 }
 
 let h = 0;
-let f = 0;
 //Keyboard controls
-let penguinUpInterval = setInterval(function(){
-}, 1000);
-let penguinDownInterval = setInterval(function(){
-}, 1000);
-let duckInterval = setInterval(function(){
-}, 1000);
-
 function keyboardControls(e) {
     switch(e.key){
         //Allows jump, but cannot float character
         case 'w': case 'ArrowUp':
             if(h < 1){
-                /*
-                let newInterval = setInterval(function(){
-                    imageHandler(rails, railObj, 30, 100, 32, 32);
-                    penguinY > 0? penguinY += 0 : null;
-                }, 100);
-            setTimeout(function(){
-                clearInterval(newInterval);
-                h = 0;
-            }, 500);
-            */
-            clearInterval(newInterval);
-            clearInterval(duckInterval);
-            penguinUpInterval = setInterval(function(){
-                ctx.clearRect(0, 0, game.width, game.height);
-                imageHandler(penguin, penguinObj, penguinX, penguinY - 20, 32, 32);
-            }, 1000);
-            setTimeout(() => {
-                clearInterval(penguinUpInterval);
-                letPenguinDown = setInterval(() => {
-                    ctx.clearRect(0, 0, game.width, game.height);
-                    imageHandler(penguin, penguinObj, penguinX, penguinY, 32, 32);
-                }, 1000);
+            h++;
+            for(let i = 0; i < 32; i++){
+                ctx.clearRect(30 , 98, 32, 32);
+                imageHandler(rails, railObj, 30, 100, 32, 32);
+                penguinY > 0? penguinY -= 1 : null;
                 
-            }, 1000);
+            }
+            for(let i = 0; i < 32; i++){
+                setTimeout(function(){
+                    penguinY > 0? penguinY += 1 : null;
+                }, 600);
+            }
+            setTimeout(function(){
+                h = 0;
+            }, 700);
             }
             break;
-        case 's': case 'ArrowDown':
-            /*if(h < 1){
-                h++;
-                for(let i = 0; i < 32; i++){
-                    ctx.clearRect(30 , 98, 32, 32);
-                    imageHandler(duckingPenguin, duckingPenguinObj, 30, 100, 32, 32);
-                    //penguinY > 0? penguinY -= 0 : null;
-                }
-                for(let i = 0; i < 32; i++){
-                    setTimeout(function(){
-                    ctx.clearRect(30 , 98, 32, 32);
-                    imageHandler(duckingPenguin, duckingPenguinObj, 30, 100, 32, 32);
-                    penguinY > 0? penguinY += 0 : null;
-                    }, 600);
-                }
-                setTimeout(function(){
-                    h = 0;
-                }, 700);*/
-                clearInterval(newInterval);
-                clearInterval(penguinUpInterval);
-                duckInterval = setInterval(function(){
-                ctx.clearRect(0, 0, game.width, game.height)
-                imageHandler(duckingPenguin, duckingPenguinObj, penguinX , penguinY, 32, 32);
-                gameLoop();
-                }, 1000);
-                break;
-                //}
+        case 's':
+        case 'ArrowDown':
+            break;
+        case 'a':
+        case 'ArrowLeft':
+            break;
+        case 'd':
+        case 'ArrowRight':
+            break;
     }
 }
 //See if key is pressed
@@ -321,7 +320,6 @@ document.addEventListener('keydown', keyboardControls);
 const playButtonSelector = document.querySelector("#play");
 //Adding listener for click
 playButtonSelector.addEventListener('click', playButton);
-
 //Select the lose area
 const loseStateSelector = document.querySelector("#lose-state");
 function playButton(){
@@ -339,11 +337,14 @@ function hitLost(objectX, objectY){
         return gameOver;
     }
 }
+
 //Trick function
+
 /*Ghost enemy function:
 Either duck, or jump to get past them alive
 */
-//Game lost function
+
+//Game over function
 function gameLost(){
     let gameIdSelector = document.querySelector('#game');
     gameIdSelector.innerHTML = 'Done';
